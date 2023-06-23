5bf45bbf7f927d14935790d743b23e7782901bfd
mune-kyun <rzknurfaizi@gmail.com>
2023-06-23 16:55:39 +0700

    add pagination

--- src/pages/api/pokemon.js
+++ src/pages/api/pokemon.js
@@ -1,14 +1,17 @@
-const itemLimit = 12;
+export const itemPerPage = 16;
 
-export const getPokemon = async () => {
+export const getPokemon = async (offset = 0) => {
   const res = await fetch(
-    `https://pokeapi.co/api/v2/pokemon?offset=0&limit=${itemLimit}`
+    `https://pokeapi.co/api/v2/pokemon?offset=${
+      offset * itemPerPage
+    }&limit=${itemPerPage}`
   );
   const json = await res.json();
 
-  return { pokemon: json.results, pageCount: json.count / itemLimit };
+  return { pokemon: json.results, pageCount: json.count / itemPerPage };
 };
 
 export default {
   getPokemon,
+  itemPerPage,
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,44 @@
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
+import { useEffect, useState } from "react";
+import { getPokemon, itemPerPage } from "./api/pokemon";
+import ReactPaginate from "react-paginate";
 
 export default function Home() {
+  const [offset, setOffset] = useState(0);
+  const [pageCount, setPageCount] = useState(1);
+  const [pokemon, setPokemon] = useState([]);
+
+  const fetchPokemon = async (offset = 0) => {
+    const { pokemon, pageCount } = await getPokemon(offset);
+    setPokemon(pokemon);
+    setPageCount(pageCount);
+  };
+
+  useEffect(() => {
+    fetchPokemon(offset);
+  }, [offset]);
+
+  const handlePageClick = (event) => {
+    setOffset(event.selected);
+  };
+
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`flex min-h-screen flex-col items-center justify-between p-24 `}
     >
-      <p>homepage</p>
+      <p>pokemon</p>
+      {pokemon.map(({ name }) => {
+        return <p key={name}>{name}</p>;
+      })}
+      <ReactPaginate
+        breakLabel="..."
+        previousLabel={"prev"}
+        nextLabel={"next"}
+        pageRangeDisplayed={5}
+        pageCount={pageCount}
+        onPageChange={handlePageClick}
+        containerClassName={"pagination"}
+        activeClassName={"active"}
+      />
     </main>
   );
 }
