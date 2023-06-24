c5944b066960e2768dccaf76214ce9118bb47ad9
mune-kyun <rzknurfaizi@gmail.com>
2023-06-25 01:52:34 +0700

    change title

--- src/components/AppTitle.js
+++ src/components/AppTitle.js
@@ -4,7 +4,9 @@ export default function AppTitle() {
   return (
     <div className="flex items-center justify-center gap-3">
       <Image src={`/pokemon-main.png`} width={30} height={30} alt="Main" />
-      <h1 className="text-[#7dca5c] text-2xl sm:text-4xl font-bold">Pokédex</h1>
+      <h1 className="text-[#7dca5c] text-2xl sm:text-4xl font-bold">
+        PokéDexu
+      </h1>
     </div>
   );
 }
