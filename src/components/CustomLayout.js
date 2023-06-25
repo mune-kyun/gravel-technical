54bf2db4d4bf1354f0a77f8a18f7e3cb60281f77
mune-kyun <rzknurfaizi@gmail.com>
2023-06-25 17:00:37 +0700

    fix: window undefined on bg

--- src/components/CustomLayout.js
+++ src/components/CustomLayout.js
@@ -1,11 +1,17 @@
 import { useWindowWidth } from "@react-hook/window-size";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 export default function CustomLayout({ children }) {
   const router = useRouter();
   const { pathname } = router;
   const widthNow = useWindowWidth();
-  const showBg = widthNow > 640 && (pathname == "/my-list" || pathname == "/");
+
+  const [showBg, setShowBg] = useState(false);
+
+  useEffect(() => {
+    setShowBg(widthNow > 640 && (pathname == "/my-list" || pathname == "/"));
+  }, [pathname, widthNow]);
 
   return (
     <div
